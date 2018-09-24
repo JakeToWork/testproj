c1669adfe0b6dd036654991c6fc97e9f3c631526
Jake <jake.tw@Jakes-MacBook-Pro.local>
2018-09-24 13:57:42 +0800

    OK

--- Modules/ChatMessageHandler.cs
+++ Modules/ChatMessageHandler.cs
@@ -23,8 +23,37 @@ namespace MasterCardServer
             var socketId = WebSocketConnectionManager.GetId(socket);
             var message =  Encoding.UTF8.GetString(buffer, 0, result.Count) ;
             var msg = JsonConvert.DeserializeObject<Message>(message);
-
-            await SendMessageAsync(msg.toSockID,msg.message);
+            var tosock = WebSocketConnectionManager.GetSocketById(socketId);
+            if(tosock.State != WebSocketState.Connecting){
+                var systemmessage = new Message
+                {
+                    fromSockID = msg.fromSockID,
+                    toSockID = msg.toSockID,
+                    messageType = Message.type.server,
+                    message = "Target is not Online"
+                };
+                using (var context = Methods.getDb())
+                { 
+                    var room = context.Rooms.Where(x=>x.SocketID==socketId);
+                    if(room.Any())
+                    { 
+                         context.Rooms.RemoveRange(room);
+                    } 
+                    var sockid = context.Players.Where(x=>x.SockID==socketId);
+                    if(sockid.Any())
+                    {
+                        var players= context.Players.Where(x=>x.roomID == sockid.Single().roomID && x.SockID!=socketId); 
+                        foreach(var p in players){
+                            await SendMessageAsync(p.SockID, "[System]Player Leaves");
+                        } 
+                        context.Players.RemoveRange(sockid);
+                    } 
+                    await context.SaveChangesAsync();
+                }
+                await SendMessageAsync(msg.fromSockID,JsonConvert.SerializeObject(systemmessage));
+            }else{
+                await SendMessageAsync(msg.toSockID,message);
+            }
         }
     }
 }
--- Modules/Message.cs
+++ Modules/Message.cs
@@ -1,7 +1,13 @@
 namespace MasterCardServer{
+
     public class Message{
+        public enum type{
+            player,
+            server
+        }
         public string fromSockID{get;set;}
         public string toSockID{get;set;}
+        public type messageType{get;set;}
         public string message{get;set;}
     }
 }
